acf44fcdea74a66a144dd04fc2e6bf33261cf977
Grishin A.R <grishinarpz@gmail.com>
2021-08-14 20:34:43 +0300

    add help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,6 @@ import config from '../package.json';
 program
   .version(config.version)
   .description(config.description)
+  .option('-f, --format [type]', 'output format', 'diff')
+  .arguments('<filepath1> <filepath2>')
   .parse(process.argv);
